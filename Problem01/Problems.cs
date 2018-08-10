a57e2cd255a303781841dd302281892ea7673bb9
Fredrik Åsgård <fredrik.asgard@avanade.com>
2018-08-10 12:49:19 +0200

    Add project files.

---
+++ Problem01/Problems.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem01
+{
+    public class Problems
+    {
+
+        /// <summary>
+        ///   Calculates the Median of a comma-separated string of values.
+        ///   The median of a discrete sequence is defined as "the value
+        ///   separating the upper half from the lower half of an ordered
+        ///   sequence".
+        /// </summary>
+        /// <remarks>
+        ///   <b>input</b> may be considered user generated, and may contain
+        ///   errors. This method will sanitize such inputs by discarding
+        ///   invalid numbers.
+        /// </remarks>
+        /// <param name="input">
+        ///   A comma-separated string of values.
+        /// </param>
+        /// <returns>
+        ///   The median value of the number sequence as an int.
+        /// </returns>
+        /// <seealso cref="https://en.wikipedia.org/wiki/Median"/>
+        public int Median( string input )
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ Tests01/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests01")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("862d9e6b-3066-46e8-baa6-2b5a09496906")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
