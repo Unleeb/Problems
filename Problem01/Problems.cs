c8f73dd740fc3377581605bd6f66b07d7907c9e4
Fredrik Åsgård <unleeb@gmail.com>
2019-05-02 13:25:55 +0200

    Add files via upload
    Improved formatting and added details to comment to clarify the case where median might be a real number instead of integer.

--- Problem01/Problems.cs
+++ Problem01/Problems.cs
@@ -1,36 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Problem01
 {
-    public class Problems
-    {
+	public class Problems
+	{
 
-        /// <summary>
-        ///   Calculates the Median of a comma-separated string of values.
-        ///   The median of a discrete sequence is defined as "the value
-        ///   separating the upper half from the lower half of an ordered
-        ///   sequence".
-        /// </summary>
-        /// <remarks>
-        ///   <b>input</b> may be considered user generated, and may contain
-        ///   errors. This method will sanitize such inputs by discarding
-        ///   invalid numbers.
-        /// </remarks>
-        /// <param name="input">
-        ///   A comma-separated string of values.
-        /// </param>
-        /// <returns>
-        ///   The median value of the number sequence as an int.
-        /// </returns>
-        /// <seealso cref="https://en.wikipedia.org/wiki/Median"/>
-        public int Median( string input )
-        {
-            return 0;
-        }
+		/// <summary>
+		///   Calculates the Median of a comma-separated string of values.
+		///   The median of a discrete sequence is defined as "the value
+		///   separating the upper half from the lower half of an ordered
+		///   sequence".
+		/// </summary>
+		/// <remarks>
+		///   <b>input</b> may be considered user generated, and may contain
+		///   errors. This method will sanitize such inputs by discarding
+		///   invalid numbers. You can assume the median will be an
+		///   integer - any lost precision is acceptable.
+		/// </remarks>
+		/// <param name="input">
+		///   A comma-separated string of values.
+		/// </param>
+		/// <returns>
+		///   The median value of the number sequence as an int.
+		/// </returns>
+		/// <seealso cref="https://en.wikipedia.org/wiki/Median"/>
+		public int Median( string input )
+		{
+			return 0;
+		}
 
-    }
+	}
 }
